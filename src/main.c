d9b84a69e64f6a4f8952dc98b8d08d85d3832332
nishuihan <nishuihan@kunlun-inc.com>
2017-09-15 17:39:15 +0800

    a

--- src/main.c
+++ src/main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>  
 
 #include "tluac-thread/tluac-thread.h"
 
+int listenFd
+
 int main() {
+	listenFd = socket(AF_INET, SOCK_STREAM, 0);  
+    fcntl(listenFd, F_SETFL, O_NONBLOCK); // set non-blocking  
+    printf("server listen fd=%d\n", listenFd);  
+	// bind & listen  
+    sockaddr_in sin;  
+    bzero(&sin, sizeof(sin));  
+    sin.sin_family = AF_INET;  
+    sin.sin_addr.s_addr = INADDR_ANY;  
+    sin.sin_port = htons(8888);  
+    bind(listenFd, (const sockaddr*)&sin, sizeof(sin));  
+    listen(listenFd, 5);
+
 	thread_new();
 	return 0;
 }
--- src/tluac-thread/tluac-thread.c
+++ src/tluac-thread/tluac-thread.c
@@ -1,32 +1,33 @@
 #include "tluac-thread.h"
+#include "tluac-epoll/tluac-epoll.h"
 
 #include "../tluac-lua/tluac-lua.h"
 
 void thread_new()
 {
 
-	struct sockaddr_in     servaddr;
-	//初始化Socket
-	if( (socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){
-		printf("create socket error: %s(errno: %d)\n",strerror(errno),errno);
-		exit(0);
-	}
-	//初始化
-	memset(&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);//IP地址设置成INADDR_ANY,让系统自动获取本机的IP地址。
-	servaddr.sin_port = htons(DEFAULT_PORT);//设置的端口为DEFAULT_PORT
-
-	//将本地地址绑定到所创建的套接字上
-	if( bind(socket_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1){
-		printf("bind socket error: %s(errno: %d)\n",strerror(errno),errno);
-		exit(0);
-	}
-	//开始监听是否有客户端连接
-	if( listen(socket_fd, 10) == -1){
-		printf("listen socket error: %s(errno: %d)\n",strerror(errno),errno);
-		exit(0);
-	}
+//	struct sockaddr_in     servaddr;
+//	//初始化Socket
+//	if( (socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){
+//		printf("create socket error: %s(errno: %d)\n",strerror(errno),errno);
+//		exit(0);
+//	}
+//	//初始化
+//	memset(&servaddr, 0, sizeof(servaddr));
+//	servaddr.sin_family = AF_INET;
+//	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);//IP地址设置成INADDR_ANY,让系统自动获取本机的IP地址。
+//	servaddr.sin_port = htons(DEFAULT_PORT);//设置的端口为DEFAULT_PORT
+//
+//	//将本地地址绑定到所创建的套接字上
+//	if( bind(socket_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1){
+//		printf("bind socket error: %s(errno: %d)\n",strerror(errno),errno);
+//		exit(0);
+//	}
+//	//开始监听是否有客户端连接
+//	if( listen(socket_fd, 10) == -1){
+//		printf("listen socket error: %s(errno: %d)\n",strerror(errno),errno);
+//		exit(0);
+//	}
 
 	/*用默认属性初始化互斥锁*/
 	pthread_mutex_init(&mut,NULL);
@@ -46,7 +47,7 @@ void thread_create(void)
 	else
 		printf("线程1被创建\n");
 
-	if((temp = pthread_create(&thread[1], NULL, thread2, NULL)) != 0)  //comment3
+	if((temp = pthread_create(&thread[1], NULL, thread1, NULL)) != 0)  //comment3
 		printf("线程2创建失败");
 	else
 		printf("线程2被创建\n");
@@ -75,28 +76,30 @@ void *thread1()
 	char    buff[4096];
 	int     n;
 	printf("======waiting for client's request======\n");
-	while(1){
-		//阻塞直到有客户端连接，不然多浪费CPU资源。
-		if( (connect_fd = accept(socket_fd, (struct sockaddr*)NULL, NULL)) == -1){
-			printf("accept socket error: %s(errno: %d)",strerror(errno),errno);
-			continue;
-		}
-		//接受客户端传过来的数据
-		while(1){
-			n = recv(connect_fd, buff, MAXLINE, 0);
-			//向客户端发送回应数据
-			//if(!fork()){ /*紫禁城*/
-			if(send(connect_fd, "Hello,you are connected!\n", 26,0) == -1)
-				perror("send error");
-			//	close(connect_fd);
-			//		exit(0);
-			//	}
-			buff[n] = '\0';
-			printf("recv msg from client: %s\n", buff);
-		}
-		close(connect_fd);
-	}
-	close(socket_fd);
+//	while(1){
+//		//阻塞直到有客户端连接，不然多浪费CPU资源。
+//		if( (connect_fd = accept(socket_fd, (struct sockaddr*)NULL, NULL)) == -1){
+//			printf("accept socket error: %s(errno: %d)",strerror(errno),errno);
+//			continue;
+//		}
+//		//接受客户端传过来的数据
+//		while(1){
+//			n = recv(connect_fd, buff, MAXLINE, 0);
+//			//向客户端发送回应数据
+//			//if(!fork()){ /*紫禁城*/
+//			if(send(connect_fd, "Hello,you are connected!\n", 26,0) == -1)
+//				perror("send error");
+//			//	close(connect_fd);
+//			//		exit(0);
+//			//	}
+//			buff[n] = '\0';
+//			printf("recv msg from client: %s\n", buff);
+//		}
+//		close(connect_fd);
+//	}
+//	close(socket_fd);
+
+	epoll_new();
 
 }
 
