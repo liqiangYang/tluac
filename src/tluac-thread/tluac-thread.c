9956fc17591224dcd3c1eb848ca47af19a28f58e
nishuihan <nishuihan@kunlun-inc.com>
2017-09-18 16:45:24 +0800

    aaaaa

--- src/tluac-thread/tluac-thread.c
+++ src/tluac-thread/tluac-thread.c
@@ -70,7 +70,7 @@ void *thread_listen(void *arg) {
 	// create & bind listen socket, and add to epoll, set non-blocking
 	InitListenSocket(l_epollFd, port);
 
-	struct context ctx = (struct prodcons) arg;
+	struct context ctx = *(struct context *)arg;
 	printf("thread listen :  %p \n", ctx);
 	thread_new(ctx);
 
@@ -78,7 +78,7 @@ void *thread_listen(void *arg) {
 }
 
 void *thread_worker(void *arg) {
-	struct context ctx = (struct prodcons) arg;
+	struct context ctx = *(struct context*) arg;
 	printf("thread :  %p \n", ctx);
 
 	epoll_new(ctx);
